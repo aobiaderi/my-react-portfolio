c74d7d86f28b7cf90f520abbc550671f4842d864
Obee123 <aobiaderi@yahoo.com>
2023-03-15 14:40:09 +0000

    updated code in projects.js

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -1,20 +1,60 @@
 
+// import React from 'react';
+// import Project from './Projects';
+// import projects from '../projects.json';
+
+// function ProjectGallery() {
+//   return (
+//     <div className="container-fluid">
+//       <div className="row justify-content-center">
+//         {projects.slice(0, 6).map((project, index) => (
+//           <div key={project.id} className="col-sm-12 col-md-6 col-lg-4 mb-4">
+//             <Project project={project} index={index} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectGallery;
+
 import React from 'react';
 import Project from './Projects';
 import projects from '../projects.json';
 
 function ProjectGallery() {
-  return (
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        {projects.slice(0, 6).map((project, index) => (
-          <div key={project.id} className="col-sm-12 col-md-6 col-lg-4 mb-4">
-            <Project project={project} index={index} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+const project1 = projects[0];
+const project2 = projects[1];
+const project3 = projects[2];
+const project4 = projects[3];
+const project5 = projects[4];
+const project6 = projects[5];
+
+return (
+<div className="container-fluid">
+<div className="row justify-content-center">
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project1} index={0} />
+</div>
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project2} index={1} />
+</div>
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project3} index={2} />
+</div>
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project4} index={3} />
+</div>
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project5} index={4} />
+</div>
+<div className="col-sm-12 col-md-6 col-lg-4 mb-4">
+<Project project={project6} index={5} />
+</div>
+</div>
+</div>
+);
 }
 
 export default ProjectGallery;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -23,33 +23,33 @@
 
 // export default Project;
 
-import React from 'react';
+// import React from 'react';
 
-function Project({ project }) {
+// function Project({ project }) {
   
-  return (
-    <div className="container-fluid">
-      <div className="row d-flex justify-content-center">
-        <div className="cardContainer">
-          <div className="card h-100">
-            {/* <img src={project.image} className="card-img-top" alt={`${project.name} screenshot`} /> */}
+//   return (
+//     <div className="container-fluid">
+//       <div className="row d-flex justify-content-center">
+//         <div className="cardContainer">
+//           <div className="card h-100">
+//             {/* <img src={project.image} className="card-img-top" alt={`${project.name} screenshot`} /> */}
             
-            <img src={`${process.env.PUBLIC_URL}/${project.image}`} className="card-img-top" alt={`${project.name} screenshot`} />
+//             <img src={`${process.env.PUBLIC_URL}/${project.image}`} className="card-img-top" alt={`${project.name} screenshot`} />
 
-            <div className="card-body">
-              <h5 className="card-title">{project.name}</h5>
-              <a href={project.site} className="btn btn-primary depl">Deployed version</a>
-              {'  '}
-              <a href={project.github} className="btn btn-secondary repos">GitHub repository</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+//             <div className="card-body">
+//               <h5 className="card-title">{project.name}</h5>
+//               <a href={project.site} className="btn btn-primary depl">Deployed version</a>
+//               {'  '}
+//               <a href={project.github} className="btn btn-secondary repos">GitHub repository</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Project;
+// export default Project;
 
 // import React from 'react';
 // // import images from '../../../public/'
@@ -75,3 +75,78 @@ export default Project;
 // }
 
 // export default Project;
+// import React from 'react';
+// import camping3 from '../images';
+// import Weather Dashboard from '../images/camping3.gif';
+// import Coding Quiz from '../images';
+// import work-day-scheduler from '../images';
+// import password_generator from '../images';
+// import my-team-webpage from '../images';
+
+
+// function Project({ project, index }) {
+// const projectIndex = `project${index + 1}`; // generate a string like 'project1' for the image object lookup
+// const image = images[projectIndex]; // look up the image object using the generated string
+
+// return (
+// <div className="container-fluid">
+// <div className="row d-flex justify-content-center">
+// <div className="cardContainer">
+// <div className="card h-100">
+// <img src={image.src} className="card-img-top" alt={`${project.name} screenshot`} />
+// <div className="card-body">
+// <h5 className="card-title">{project.name}</h5>
+// <a href={project.site} className="btn btn-primary depl">Deployed version</a>
+// {' '}
+// <a href={project.github} className="btn btn-secondary repos">GitHub repository</a>
+// </div>
+// </div>
+// </div>
+// </div>
+// </div>
+// );
+// }
+
+// export default Project;
+
+import React from 'react';
+import camping3 from '../images/camping3.gif';
+import codingQuiz from '../images/Coding Quiz.gif';
+import workDayScheduler from '../images/work-day-scheduler.gif';
+import passwordGenerator from '../images/password_generator.png';
+import myTeamWebpage from '../images/my-team-webpage.png';
+import weatherDashboard from '../images/Weather Dashboard.gif';
+
+const images = {
+  project1: camping3,
+  project2: codingQuiz,
+  project3: workDayScheduler,
+  project4: passwordGenerator,
+  project5: myTeamWebpage,
+  project6:weatherDashboard
+};
+
+function Project({ project, index }) {
+  const projectIndex = `project${index + 1}`; // generate a string like 'project1' for the image object lookup
+  const image = images[projectIndex]; // look up the image object using the generated string
+
+  return (
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
+        <div className="cardContainer">
+          <div className="card h-100">
+            <img src={image} className="card-img-top" alt={`${project.name} screenshot`} />
+            <div className="card-body">
+              <h5 className="card-title">{project.name}</h5>
+              <a href={project.site} className="btn btn-primary depl">Deployed version</a>
+              {' '}
+              <a href={project.github} className="btn btn-secondary repos">GitHub repository</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
